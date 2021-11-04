09a44d99b36b0873033c5af47650c845b5df5ee8
NatalinaR <93716725+NatalinaR@users.noreply.github.com>
2021-11-04 20:40:07 +0300

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,28 +1,3 @@
-// const d1 = Image ()
-// const d2
-// const d3
-// const d4
-// const d5
-// const d6
-// const d7
-// const d8
-// const d9
-// const d10
-// const d11
-// const d12
-// const d13
-// const d14
-// const d15
-// const d16
-// const d17
-// const d18
-// const d20
-// const d21
-// const d22
-// const d23
-// const d24
-// const d25
-
 window.onload = function(){
     let buttons = document.querySelectorAll('.look1');
  
